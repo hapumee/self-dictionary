17398ec67da7f057bbc90a78c951369f48b81688
youngsunmin <youngsun.min@navercorp.com>
2019-11-14 19:27:18 +0900

    Refactor code of getting the word list

--- server/routes/words.js
+++ server/routes/words.js
@@ -4,10 +4,13 @@ const _ = require('underscore');
 const fs = require('fs');
 const WORD_LIST_API = 'data/words.list.json';
 
-// get word list
-router.get("/", async function(req, res, next) {
-    const response = await new Promise((resolve, reject) => {
-        fs.readFile(WORD_LIST_API, 'utf8', function(err, data) {
+/**
+ * get the word list
+ * @returns {Promise.<*>}
+ */
+const getWordList = async () => {
+    return await new Promise((resolve, reject) => {
+        fs.readFile(WORD_LIST_API, 'utf8', (err, data) => {
             if (err) {
                 reject(err);
             } else {
@@ -15,41 +18,37 @@ router.get("/", async function(req, res, next) {
             }
         });
     });
-    // console.log(response);
-    res.send(response);
+};
+
+// get word list
+router.get("/", async function(req, res, next) {
+    getWordList().then((response) => {
+        res.send(response);
+    });
 });
 
 // add new word on the list
 router.post("/", async function(req, res, next) {
     // console.log("input:", req.body);
 
-    const response = await new Promise((resolve, reject) => {
-        fs.readFile(WORD_LIST_API, 'utf8', function(err, data) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
+    getWordList().then((response) => {
+        let words = JSON.parse(response); // {Array}
+        let elWithMaxId = _.max(words['result_data'], function(word) {
+            return word.id;
         });
-    });
+        let inputId = !_.isEmpty(elWithMaxId) ? parseInt(elWithMaxId.id) + 1 : 0;
+        let inputWord = req.body.word;
 
-    let words = JSON.parse(response); // {Array}
-    let elWithMaxId = _.max(words['result_data'], function(word) {
-        return word.id;
-    });
-    let inputId = parseInt(elWithMaxId.id) + 1;
-    let inputWord = req.body.word;
+        words['result_data'].push({
+            id: inputId,
+            word: inputWord
+        });
+        // console.log(words);
 
-    words['result_data'].push({
-        id: inputId,
-        word: inputWord
-    });
-    // console.log(words);
-
-    fs.writeFile(WORD_LIST_API, JSON.stringify(words), 'utf-8', function(err) {
-        if (err) throw err;
-
-        res.sendStatus(200);
+        fs.writeFile(WORD_LIST_API, JSON.stringify(words), 'utf-8', function(err) {
+            if (err) throw err;
+            res.sendStatus(200);
+         });
     });
 });
 
@@ -57,28 +56,20 @@ router.post("/", async function(req, res, next) {
 router.delete("/:id", async function(req, res, next) {
     // console.log(req.params);
 
-    const response = await new Promise((resolve, reject) => {
-        fs.readFile(WORD_LIST_API, 'utf8', function(err, data) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
+    getWordList().then((response) => {
+        let words = JSON.parse(response); // {Array}
+        let paramId = parseInt(req.params.id);
+        let targetId = _.findIndex(words['result_data'], function(word) {
+            return _.isEqual(word.id, paramId);
         });
-    });
 
-    let words = JSON.parse(response); // {Array}
-    let paramId = parseInt(req.params.id);
-    let targetId = _.findIndex(words['result_data'], function(word) {
-        return _.isEqual(word.id, paramId);
-    });
+        words['result_data'].splice(targetId, 1);
+        // console.log(words);
 
-    words['result_data'].splice(targetId, 1);
-
-    fs.writeFile(WORD_LIST_API, JSON.stringify(words), 'utf-8', function(err) {
-        if (err) throw err;
-
-        res.sendStatus(200);
+        fs.writeFile(WORD_LIST_API, JSON.stringify(words), 'utf-8', function(err) {
+            if (err) throw err;
+            res.sendStatus(200);
+        });
     });
 });
 
